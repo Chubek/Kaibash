230f079d8bd27f007d914a89fad9247bc4447800
Chubak Bidpaa <chubakbidpaa@gmail.com>
2023-12-23 13:23:40 +0330

    renamed

--- tables.h
+++ tables.h
@@ -1,19 +1,8 @@
 #ifndef TABLES_H_
 #define TABLES_H_
 
-#include <fcntl.h>
-#include <limits.h>
-#include <signal.h>
-#include <stdarg.h>
-#include <stdbool.h>
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
-#include <sys/stat.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <termios.h>
-#include <unistd.h>
 
 /*======== Memory allocation table =======*/
 
