9d5478577cf8d0fc17e5f9356f255f0607ab9de1
Chubak Bidpaa <chubakbidpaa@gmail.com>
2023-12-23 16:24:52 +0330

    formatted, added macro

--- wrapper.c
+++ wrapper.c
@@ -13,6 +13,7 @@
 #include "table.h"
 
 #define FDSTR_MAX 32
+#define NPIPE 2
 
 inline int
 io_open_read (char *path)
@@ -69,49 +70,49 @@ duplicate_fd_err (int fd)
 }
 
 inline int
-execute_program(char* prog, char* args[MAX_ARG])
+execute_program (char *prog, char *args[MAX_ARG])
 {
-	execvp(prog, args);
-	perror("execvp");
-	exit(EXIT_FAILURE);
+  execvp (prog, args);
+  perror ("execvp");
+  exit (EXIT_FAILURE);
 }
 
 inline bool
-wait_child(pid_t proc_id, int* result)
+wait_child (pid_t proc_id, int *result)
 {
-	pid_t p;
+  pid_t p;
 
-	if ((p = waitpid(WAIT_ANY, &result, WUNTRACED | WNOHAND)) < 0)
-	{
-		perror("waitpid");
-		exit(EXIT_FAILURE);
-	}
+  if ((p = waitpid (WAIT_ANY, &result, WUNTRACED | WNOHAND)) < 0)
+    {
+      perror ("waitpid");
+      exit (EXIT_FAILURE);
+    }
 
-	return p - proc_id == 0;
+  return p - proc_id == 0;
 }
 
 inline void
-pipe_output(int io_pipe[NPIPE], int err_pipe[NPIPE], int out_fd, int err_fd)
+pipe_output (int io_pipe[NPIPE], int err_pipe[NPIPE], int out_fd, int err_fd)
 {
-	close(io_pipe[0]);
-	close(err_pipe[0]);
+  close (io_pipe[0]);
+  close (err_pipe[0]);
 
-	if (dup2(io_pipe[1], out_fd) < 0)
-		perror("dup2");
+  if (dup2 (io_pipe[1], out_fd) < 0)
+    perror ("dup2");
 
-	if (dup2(err_pipe[1], err_fd) < 0)
-		perror("dup2");
+  if (dup2 (err_pipe[1], err_fd) < 0)
+    perror ("dup2");
 }
 
 inline void
-pipe_input(int io_pipe[NPIPE], int err_pipe[NPIPE], int in_fd, int err_fd)
+pipe_input (int io_pipe[NPIPE], int err_pipe[NPIPE], int in_fd, int err_fd)
 {
-	close(io_pipe[1]);
-	close(err_pipe[1]);
+  close (io_pipe[1]);
+  close (err_pipe[1]);
 
-	if (dup2(io_pipe[0], in_fd) < 0)
-		perror("dup2");
+  if (dup2 (io_pipe[0], in_fd) < 0)
+    perror ("dup2");
 
-	if (dup2(err_pipe[0], err_fd) < 0)
-		perror("dup2");
+  if (dup2 (err_pipe[0], err_fd) < 0)
+    perror ("dup2");
 }
